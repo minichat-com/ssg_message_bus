9fe050d2c2cdaa975c40e78c1e3db8fa1c716872
Illia Kasianenko <i.kasianenko@gmail.com>
2024-11-20 10:55:24 +0100

    Add destination1 to filters

--- lib/ssg_message_bus/gcp_pub_sub/consumer.rb
+++ lib/ssg_message_bus/gcp_pub_sub/consumer.rb
@@ -25,7 +25,7 @@ module SSGMessageBus
       attr_accessor(*ATTRS)
 
       def default_filter
-        (@destination ? "attributes.destination = \"#{@destination}\"" : nil)
+        (@destination ? "attributes.destination = \"#{@destination}\" OR attributes.destination1 = \"#{@destination}\"" : nil)
       end
 
       def initialize(**kwargs)
